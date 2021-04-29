a921ea87e7bd0b360bb1804b42e064074bd59160
Stefano-Para <stefano.zina@gmail.com>
2021-04-29 14:53:40 +0200

    fix esercizio

--- js/script.js
+++ js/script.js
@@ -1,21 +1,31 @@
-// Scrivi un programma che stampi i numeri da 1 a 100
-// var prova = 0;
+//  Scrivi un programma che stampi i numeri da 1 a 100
+//  var prova = 0;
 
-for (i = 1; i <= 100; i++) {
+ for (i = 1; i <= 100; i++) {
     
-    // per i multipli di 3 stampi "Fizz" al posto del numero.
-        if ((i % 3) == 0) {
-            console.log(i + " fizz");
-        }
-    // per i multipli di 5 stampi Buzz.
-        else if ((i % 5) == 0) {
-            console.log(i + " buzz")
-        }
-    // Per i numeri che sono sia multipli di 3 che di 5 stampi FizzBuzz.
+    //   Per i numeri che sono sia multipli di 3 che di 5 stampi FizzBuzz.    
         if (((i % 3) == 0) && ((i % 5) == 0)) {
             console.log(i + " FizzBuzz")
         }
-}
+
+    //   per i multipli di 3 stampi "Fizz" al posto del numero.
+
+        else if ((i % 3) == 0) {
+            console.log(i + " fizz");
+         }
+
+    //   per i multipli di 5 stampi Buzz.
+        else if ((i % 5) == 0) {
+            console.log(i + " buzz")
+        }
+        else {
+            console.log(i)
+        }
+     
+ }
+
+
+
 
 
 
